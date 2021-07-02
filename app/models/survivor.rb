0bec730a4be9a207c4fb9a4dbf86c44ac7eba5b8
Christian Freitas <chrfreitas@gmail.com>
2021-07-02 18:19:35 -0300

    First test in survivor model

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,2 +1,3 @@
 class Survivor < ApplicationRecord
+  validates :name, presence: true
 end
--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Survivor, type: :model do
-  
+  context "validations" do
+    it { should validate_presence_of(:name) }
+  end
 end
